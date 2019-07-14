25e4c66b2bf4506bae0c8a02db0c41509ec6d890
Avisa Poshtkouhi <avisa_poshtkouhi@yahoo.com>
2019-07-14 14:28:36 +1000

    Initial Commit

---
+++ CoreDataIntermediateTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataIntermediateTraining
+//
+//  Created by Avisa on 14/7/19.
+//  Copyright © 2019 Avisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
